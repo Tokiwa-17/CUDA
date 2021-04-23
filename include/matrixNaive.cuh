b620cb703ba652eb7f806e342cc1b279dc1e40d5
Tokiwa-17 <74098135+Tokiwa-17@users.noreply.github.com>
2021-04-23 21:38:22 +0800

    Update matrixNaive.cuh

--- include/matrixNaive.cuh
+++ include/matrixNaive.cuh
@@ -3,9 +3,9 @@
 #include <cuda_runtime.h>
 #define BLOCK_SIZE 32
 
-class matrixNaive{
 
-public:
-    template<typename T>
-    void gpuMatrixMul(T* d_A, T* d_B, T* d_C, int m, int n, int k);
-};
+void gpuMatrixMul(T* d_A, T* d_B, T* d_C, int m, int n, int k);
+
+
+
+
