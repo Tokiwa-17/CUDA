2c00dd1046e38ee0c6ac8cdf10e07a2e449d15a4
Tokiwa-17 <74098135+Tokiwa-17@users.noreply.github.com>
2021-04-25 01:58:45 +0800

    Create matrixTranspose.cu

---
+++ src/matrixTranspose.cu
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include "../include/config.cuh"
+#include "../include/matrixTranspose.cuh"
+
+__global__ void matrixNaiveTrans(int *out, int *in, const int nx, const int ny){
+    // coordinate(ix, iy)
+    unsigned int ix = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int iy = blockIdy.y * blockDim.y + threadIdx.y;
+
+    if(ix < nx && iy < ny)
+        out[ix * ny + iy] = in[iy * nx + ix];
+}
+
+__global__ void matrixTranspose(int *out, int *in, int nx, int ny){
+    __shared__ float tile[BDIMY][BDIMX];
+    unsigned int ix, iy, ti, to;
+    //计算线程的全局坐标
+    ix = blockIdx.x * blockDim.x + threadIdx.x;
+    iy = blockIdx.y * blockDim.y + threadIdx.y;
+    ti = iy * nx + ix;
+
+    // bidx表示线程在这个线程块中的位置，计算新的坐标位置
+    unsigned int bidx, irow, icol, out_idx;
+    bidx = threadIdx.y * blockDim.x + threadIdx.x;
+    irow = bidx / blockDim.y;
+    icol = bidx % blockDim.y;
+
+    // 计算转置后的全局坐标
+    ix = blockIdx.y * blockDim.y + icol;
+    iy = blockIdx.x * blockDim.x + irow;
+    out_idx = iy * ny + ix;
+
+    if(ix < nx && iy < ny){
+        tile[threadIdx.y][threadIdx.x] = in[out_idx];
+        __syncthreads();
+        out[out_idx] = tile[icol][irow];
+    }
+
+    ix = blockIdx.y * 
+
+}
