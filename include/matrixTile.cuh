4eeffd260103d3039218d74888daf48f89f99fe8
Tokiwa-17 <74098135+Tokiwa-17@users.noreply.github.com>
2021-04-24 17:11:12 +0800

    Update matrixTile.cuh

--- include/matrixTile.cuh
+++ include/matrixTile.cuh
@@ -1,6 +1,79 @@
-#pragma once
 #include <stdio.h>
 #include <cuda_runtime.h>
+#include "../include/config.cuh"
+#include "../include/matrixTile.cuh"
 
 
-__global__ void gpuMatrixMulTile(int* d_A, int* d_B, int* d_C, int m, int n, int k);
+//矩阵的大小设置成TILE_SIZE 的倍数
+/*__global__ void gpuMatrixMulTile(int* d_A, int* d_B, int* d_C, int m, int n, int k){
+    
+    __shared__ int A_tile[TILE_SIZE][TILE_SIZE];
+    __shared__ int B_tile[TILE_SIZE][TILE_SIZE];
+
+    int bx = blockIdx.x, by = blockIdx.y;
+    int tx = threadIdx.x, ty = threadIdx.y;
+
+    //illustrate :https://cnugteren.github.io/tutorial/pages/page4.html
+    // A是横着的条, aBegin和aEnd分别是Tile第一行的开始和结束, 每循环一次横着移动一个Tile
+    int aBegin = blockIdx.y * TILE_SIZE * n;
+    int aEnd = aBegin + n - 1;
+    int aStride = TILE_SIZE;
+
+    // B是竖着的条, bBegin指向Tile第一列的开始，每循环一次竖着移动一个Tile
+    int bBegin = TILE_SIZE * bx;
+    int bStride = TILE_SIZE * k;
+
+    int accu = 0;
+
+    // 计算C的一个Tile
+    for(int i = aBegin, j = bBegin; i <= aEnd; i += aStride, j += bStride){
+        //load share memory
+        //从Tile中取出一个点放到共享内存中
+        A_tile[ty][tx] = d_A[i + n * ty + tx];
+        B_tile[tx][ty] = d_B[j + k * tx + ty];
+
+        __syncthreads();
+
+        for(int k = 0;k < TILE_SIZE; k++)
+            accu += A_tile[ty][k] * B_tile[k][tx];
+        
+        __syncthreads();
+    }
+    //A中横着的一行和B中竖着的一列累加完毕放到C中对应位置
+    int cIdx = k * TILE_SIZE * by + TILE_SIZE * bx;
+    d_C[cIdx + k * ty + tx] = accu;
+}
+*/
+
+
+__global__ void gpuMatrixMulTile(int* A, int* B, int* C, int m, int n, int k){
+
+    // Thread identifiers
+    int row = threadIdx.x, col = threadIdx.y;
+    int globalRow = TILE_SIZE * blockIdx.x + row;
+    int globalCol = TILE_SIZE * blockIdx.y + col;
+
+    // local memory to fit a tile
+    __shared__ int ATile[TILE_SIZE][TILE_SIZE];
+    __shared__ int BTILE[TILE_SIZE][TILE_SIZE];
+    
+    int acc = 0;
+
+    int numTiles = k / TILE_SIZE;
+    for(int t = 0; t < numTiles; t++){
+        //t指A是横着的第几个tile或B是竖着的第几个tile
+        //load one tile of A and B into share memory
+        int tileRow = TILE_SIZE * t + row;
+        int tileCol = TILE_SIZE * t + col;
+        ATile[col][row] = A[tileCol * m + globalRow];
+        BTile[col][row] = B[globalCol * k + tileRow];
+
+        __syncthreads();
+
+        for(int w = 0; w < TILE_SIZE; w++)
+            acc += ATile[k][row] * BTile[col][k];
+
+        __syncthreads();
+    }
+    C[globalRow + globalCol * m] = acc;
+}
