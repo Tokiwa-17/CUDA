99f65977cdf1606bb261c3935ef67090c42c28d5
Tokiwa-17 <74098135+Tokiwa-17@users.noreply.github.com>
2021-04-24 18:18:16 +0800

    Update main.cu

--- main.cu
+++ main.cu
@@ -80,7 +80,7 @@ int main(int argc, char ** argv){
     checkResult(h_C, h_odata, m * k);
 
     // GPU Matrix multiplication by tile, optimized by WPT
-    block.x = TILE_SIZE / WPT , block.y = TILE_SIZE;
+    block.x = TILE_SIZE, block.y = TILE_SIZE;
     grid.x = m, grid.y = n;
     iStart = cpuSecond();
     gpuMatrixMulTileWPT<<<grid, block>>>(d_A, d_B, d_C, m, n, k);
