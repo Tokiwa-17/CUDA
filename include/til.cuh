096eca1596b2c0e958deb18e4b6ffa363d593c8d
Tokiwa-17 <74098135+Tokiwa-17@users.noreply.github.com>
2021-04-24 22:13:54 +0800

    Update til.cuh

--- include/til.cuh
+++ include/til.cuh
@@ -1,49 +1,91 @@
-#pragma once
-#include <time.h>
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include "../include/til.cuh"
+#include "../include/config.cuh"
+
+void cpuMatrixMul(int *h_A, int * h_B, int* h_C, int m, int n, int k){
+    for(int i = 0;i < m;i++)
+        for(int j = 0;j < k;j++){
+            int sum = 0;
+            for(int l = 0;l < n;l++)
+                sum += h_A[i * n + l] * h_B[l * k + j];
+            h_C[i * k + j] = sum;
+        }
+}
+
 #ifdef _WIN32
-#	include <windows.h>
-#else
-#	include <sys/time.h>
+int gettimeofday(struct timeval* tp, void* tzp){
+    time_t clock;
+    struct tm tm;
+    SYSTEMTIME wtm;
+    GetLocalTime(&wtm);
+    tm.tm_year = wtm.wYear - 1900;
+    tm.tm_mon = wtm.wMonth - 1;
+    tm.tm_mday = wtm.wDay;
+    tm.tm_hour = wtm.wHour;
+    tm.tm_min = wtm.wMinute;
+    tm.tm_sec = wtm.wSecond;
+    tm.tm_isdst = -1;
+    clock = mktime(&tm);
+    tp->tv_sec = clock;
+    tp->tv_usec = wtm.wMilliseconds * 1000;
+    return (0);
+}
 #endif
-#ifdef _WIN32
-#endif
-#define CHECK(call){\
-  const cudaError_t error=call;\
-  if(error!=cudaSuccess){\
-      printf("ERROR: %s:%d,",__FILE__,__LINE__);\
-      printf("code:%d,reason:%s\n",error,cudaGetErrorString(error));\
-      exit(1);\
-  }\
+
+double cpuSecond(){
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+    return((double)tp.tv_sec + (double)tp.tv_usec * 1e-6);
+}
+
+void initialData(float* ip, int size){
+    //generate different seed for random number
+    time_t t;
+    srand((unsigned)time(&t));
+    for (int i = 0;i < size;i++){
+        ip[i] = (float)(rand() & 0xffff) / 1000.0f;
+    }
+}
+
+void initialDataInt(int* ip, int size){
+    time_t t;
+    srand((unsigned)time(&t));
+    for (int i = 0; i < size; i++){
+        ip[i] = int((rand() % 100) - 50);
+    }
+}
+
+void printMatrix(int* C, const int nx, const int ny){
+    int* ic = C;
+    printf("Matrix<%d,%d>:\n", ny, nx);
+    for (int i = 0;i < ny;i++){
+        for (int j = 0;j < nx;j++){
+            printf("%6d ", C[j]);
+        }
+        ic += nx;
+        printf("\n");
+    }
 }
 
 
-/*
-*********************************************************************
-function name: cpuMatrixMul
-description: Multiplication two matrix in CPU.
-parameters: 
-    &h_A CPU host pointer to a (m, n) matrix (A)
-    &h_B CPU host pointer to a (n, k) matrix (B)
-    &h_C CPU host output pointer to a (m, k) matrix (C) 
-    to store the result
-return: none
-*********************************************************************
-*/
-void cpuMatrixMul(int *h_A, int * h_B, int* h_C, int m, int n, int k);
+void initDevice(int devNum){
+    int dev = devNum;
+    cudaDeviceProp deviceProp;
+    CHECK(cudaGetDeviceProperties(&deviceProp, dev));
+    printf("Using device %d: %s\n", dev, deviceProp.name);
+    CHECK(cudaSetDevice(dev));
 
+}
 
-int gettimeofday(struct timeval* tp, void* tzp);
-//#endif
-
-double cpuSecond();
-
-void initialData(float* ip, int size);
-
-void initialDataInt(int* ip, int size);
-
-void printMatrix(int* C, const int nx, const int ny);
-
-void initDevice(int devNum);
-
-void checkResult(int* hostRef, int* gpuRef, const int N);
-
+void checkResult(int* hostRef, int* gpuRef, const int N){
+    double epsilon = 1.0E-8;
+    for (int i = 0;i < N;i++){
+        if (abs(hostRef[i] - gpuRef[i]) > epsilon){
+            printf("Results do not match!\n");
+            printf("%d(hostRef[%d] )!= %d(gpuRef[%d])\n", hostRef[i], i, gpuRef[i], i);
+            return;
+        }
+    }
+    printf("Check result success!\n");
+}
