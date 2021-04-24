f05a37bbe42b80ed1410586b4468d6bbc5c53749
Tokiwa-17 <74098135+Tokiwa-17@users.noreply.github.com>
2021-04-24 18:19:29 +0800

    Update main.cu

--- main.cu
+++ main.cu
@@ -66,8 +66,8 @@ int main(int argc, char ** argv){
     checkResult(h_C, h_odata, m * k);
 
     // GPU Matrix multiplication by tile
-    block.x = 16, block.y = 16;
-    grid.x = k / 16, grid.y = m / 16;
+    block.x = TILE_SIZE, block.y = TILE_SIZE;
+    grid.x = k / TILE_SIZE, grid.y = m / TILE_SIZE;
     iStart = cpuSecond();
     gpuMatrixMulTile<<<grid, block>>>(d_A, d_B, d_C, m, n, k);
     CHECK(cudaDeviceSynchronize());
@@ -80,8 +80,8 @@ int main(int argc, char ** argv){
     checkResult(h_C, h_odata, m * k);
 
     // GPU Matrix multiplication by tile, optimized by WPT
-    block.x = TILE_SIZE, block.y = TILE_SIZE;
-    grid.x = m, grid.y = n;
+    block.x = TILE_SIZE / WPT, block.y = TILE_SIZE;
+    grid.x = k / TILE_SIZE, grid.y = m / TILE_SIZE;
     iStart = cpuSecond();
     gpuMatrixMulTileWPT<<<grid, block>>>(d_A, d_B, d_C, m, n, k);
     CHECK(cudaDeviceSynchronize());
