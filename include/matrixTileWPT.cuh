5badaba874c35f1c856dd06bd7a20cd9e8f9edc8
Tokiwa-17 <74098135+Tokiwa-17@users.noreply.github.com>
2021-04-24 22:43:04 +0800

    Update matrixTileWPT.cuh

--- include/matrixTileWPT.cuh
+++ include/matrixTileWPT.cuh
@@ -4,3 +4,5 @@
 
 
 __global__ void gpuMatrixMulTileWPT(int* d_A, int* d_B, int* d_C, int m, int n, int k);
+
+__global__ void gpuMatrixMulTileWPTop4(int *d_A, int *d_B, int *d_C, int m, int n, int k);
