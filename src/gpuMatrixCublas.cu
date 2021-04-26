f345acaee8af0e965e51baae730a17f404caa093
Tokiwa-17 <74098135+Tokiwa-17@users.noreply.github.com>
2021-04-26 13:02:07 +0800

    Update gpuMatrixCublas.cu

--- src/gpuMatrixCublas.cu
+++ src/gpuMatrixCublas.cu
@@ -11,14 +11,9 @@ void gpuMatrixCublas(float* A, float* B, float* C, int lda, int ldb, int ldc,
     cublasHandle_t handle;
     cublasCreate(&handle);
     
-    /*if(stat != CUBLAS_STATUS_SUCCESS){
-        printf("CUBLAS initialization error!\n");
-        return;
-    }*/
-    
     double iStart = cpuSecond();
     cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, k, n, 
         &alpha, B, ldb, A, lda, &beta, C, ldc);
     double iElaps = cpuSecond() - iStart;
-    printf("gpu Matrix Benchmark\t\telapsed %f sec.\n", iElaps);
+    printf("gpu Matrix Benchmark(Cublas)\telapsed %f sec.\n", iElaps);
 }
