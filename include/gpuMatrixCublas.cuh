ddb6adaf1e253b0c5af44a41da06f070b2ca6beb
Tokiwa-17 <74098135+Tokiwa-17@users.noreply.github.com>
2021-04-26 12:07:30 +0800

    Update and rename cublas.cuh to gpuMatrixCublas.cuh

--- include/gpuMatrixCublas.cuh
+++ include/gpuMatrixCublas.cuh
@@ -3,4 +3,4 @@
 #include <cuda_runtime.h>
 #include "cublas_v2.h"
 
-void cublas(int* d_A, int* d_B, int* d_C, int m, int n, int k);
+void gpuMatrixCublas(int* d_A, int* d_B, int* d_C, int m, int n, int k);
