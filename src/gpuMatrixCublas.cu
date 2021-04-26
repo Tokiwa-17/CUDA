e40f3c2e65a660cc8ff23411cd2ba3e923e2e536
Tokiwa-17 <74098135+Tokiwa-17@users.noreply.github.com>
2021-04-26 12:45:59 +0800

    Update gpuMatrixCublas.cu

--- src/gpuMatrixCublas.cu
+++ src/gpuMatrixCublas.cu
@@ -7,14 +7,15 @@
 void gpuMatrixCublas(float* A, float* B, float* C, int lda, int ldb, int ldc,
                      int m, int n, int k, float alpha, float beta){
     
-    cudaStatus_t stat;
+    //cudaStatus_t stat;
     cublasHandle_t handle;
-    stat = cublasCreate(&handle);
+    cublasCreate(&handle);
     
-    if(stat != CUBLAS_STATUS_SUCCESS){
+    /*if(stat != CUBLAS_STATUS_SUCCESS){
         printf("CUBLAS initialization error!\n");
         return;
-    }
+    }*/
+    
     double iStart = cpuSecond();
     cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, k, n, 
         &alpha, B, ldb, A, lda, &beta, C, ldc);
