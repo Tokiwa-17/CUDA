4e67e9ec0f213dd431372397bf02ac5a970c8ad6
Tokiwa-17 <74098135+Tokiwa-17@users.noreply.github.com>
2021-04-27 17:36:47 +0800

    Update matrixTranspose.cuh

--- include/matrixTranspose.cuh
+++ include/matrixTranspose.cuh
@@ -1,27 +1,14 @@
 #pragma once
 #include <stdio.h>
 #include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#include <cublas_v2.h>
+#include <cuda.h>
+#include <cmath>
+#include <cassert>
 
-/*
-*********************************************************************
-function name : matrixNaiveTrans
-description : a naive complementation of transpose
-parameters :
-int *out, int *in, int nx, int ny
-return: none
-*********************************************************************
-*/
-__global__ void matrixNaiveTrans(int *out, int *in, const int nx, const int ny);
-/*
-*********************************************************************
-function name : matrixTranspose
-description : derive transpose of a matrix by share memory
-parameters :
+__global__ void intPtrToFloatPtr(int *in, float* out, unsigned int m, unsigned int n);
 
-steps:
-1. 从全局内存中读取块的一行，并向共享内存中写入一行
-2. 从共享内存中读取一列，并向全局内存中写入块中的一行
-return: none
-*********************************************************************
-*/
-__global__ void matrixTranspose(int *out, int *in, int nx, int ny);
+__global__ void floatPtrToIntPtr(float *in, int* out, unsigned int m, unsigned int n);
+
+__global__ void matrixTranspose(int *A, int *B, int m, int n);
