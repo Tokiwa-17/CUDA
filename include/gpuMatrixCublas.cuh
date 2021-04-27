a947b8f21bb251e0a350eab4a51799344707aa11
Tokiwa-17 <74098135+Tokiwa-17@users.noreply.github.com>
2021-04-27 18:52:05 +0800

    Update gpuMatrixCublas.cuh

--- include/gpuMatrixCublas.cuh
+++ include/gpuMatrixCublas.cuh
@@ -8,9 +8,5 @@
 #include <cmath>
 #include <cassert>
 
-__global__ void intPtrToFloatPtr(int *in, float* out, unsigned int m, unsigned int n);
-
-__global__ void floatPtrToIntPtr(float *in, int* out, unsigned int m, unsigned int n);
-
 void gpuMatrixCublas(int* A, int* B, int* C, int lda, int ldb, int ldc,
     int m, int n, int k, float alpha, float beta);
