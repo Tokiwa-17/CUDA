66a5c72cd3481e77f7cddb3c79e839de9c656bc1
Tokiwa-17 <74098135+Tokiwa-17@users.noreply.github.com>
2021-04-27 19:04:25 +0800

    Update matrixTranspose.cu

--- src/matrixTranspose.cu
+++ src/matrixTranspose.cu
@@ -8,6 +8,9 @@ void matrixTranspose(int *A, int *B, int m, int n){
     cublasHandle_t handle;
     cublasCreate(&handle);
 
+    printMatrix(A, m, n);
+    printMatrix(B, m, n);
+
     int *d_A, *d_B;
     CHECK(cudaMalloc((void **)&d_A, sizeof(int) * (m * n)));
     CHECK(cudaMalloc((void **)&d_B, sizeof(int) * (m * n)));
@@ -25,6 +28,16 @@ void matrixTranspose(int *A, int *B, int m, int n){
     float alpha = 0.f, beta = 1.f;
 
     cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_N, m, n, n, &alpha, f_B, m, f_A, m, &beta, f_B, m);
+
+    //********DEBUG********
+    float* testA, *testB;
+    testA = (float*)malloc(sizeof(float) * (m * n));
+    testB = (float*)malloc(sizeof(float) * (m * n));
+    CHECK(cudaMemcpy(testA, f_A, sizeof(float) * (m * n), cudaMemcpyDeviceToHost));
+    CHECK(cudaMemcpy(testB, f_B, sizeof(float) * (m * n), cudaMemcpyDeviceToHost));
+    printMatrix(testA, m, n);
+    printMatrix(testB, m, n);
+    //*********************/
 
     floatPtrToIntPtr<<<grid, block>>>(f_B, d_B, m, n);
 
