8c50ca7a53681b8f86472f5f0e7e189454cc3be3
Tokiwa-17 <74098135+Tokiwa-17@users.noreply.github.com>
2021-06-01 23:18:41 +0800

    Create matrixPrefetch.cu

---
+++ src/matrixPrefetch.cu
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include "../include/config.cuh"
+#include "../include/matrixPrefetch.cuh"
+
+__global__ void gpuMatrixMulPrefetch(int* d_A, int* d_B, int* d_C, int m, int n, int k){
+    int bx = blockIdx.x, by = blockIdx.y;
+    int tx = threadIdx.x, ty = threadIdx.y;
+    __shared__ A_tile[TILE_SIZE][TILE_SIZE];
+    __shared__ A_tileNxt[TILE_SIZE][TILE_SIZE];
+    
+    //register for result of C at each thread
+    int ctmp[TILE_SIZE] = {0};
+
+    int aBegin = n * TILE_SIZE * by;
+    int aEnd = aBegin + n - 1;
+    int aStride = TILE_SIZE;
+    
+    int bBegin = TILE_SIZE * VEC_SIZE * bx;
+    int bStride = TILE_SIZE * k;
+
+    int *cur = A_tile;
+    int *nxt = A_tileNxt;
+    for(int i = 0;i < TILE_SIZE / VEC_SIZE;i++)
+        cur[(i * VEC_SIZE + ty) + TILE_SIZE * tx] = d_A[aBegin + K * (i * VEC_SIZE + ty) + tx];
+    
+    __syncthreads();
+
+    for(int a = aBegin, b = bBegin; a <= aEnd; a += aStride, b += bStride){
+        if(a + aStride <= aEnd){
+            for(int i = 0;i < TILE_SIZE / VEC_SIZE; i++)
+                nxt[(i * VEC_SIZE) + ty + TILE_SIZE * tx] = d_A[a + n * (i * VEC_SIZE + ty) + tx + aStride];
+        }
+        int *aptr = cur;
+        int *bptr = &d_B[b + TILE_SIZE * ty + tx];
+
+        for(int i = 0;i < TILE_SIZE;i++){
+            int bval = *bptr;
+            for(int j = 0;j < TILE_SIZE;j++)
+                cval[j] += aptr[j] * bval;
+            aptr += TILE_SIZE;
+            bptr += TILE_SIZE；
+        }
+        __syncthreads();
+
+        int *tmp = cur;
+        cur = nxt;
+        nxt = tmp;
+    }
+    int c = k * TILE_SIZE * by + TILE_SIZE * VEC_SIZE * bx;
+    c += TILE_SIZE * ty + tx;
+    for(int i = 0;i < TILE_SIZE;i++){
+        h_C[c] = cval[i];
+        c += k;
+    }
+}
